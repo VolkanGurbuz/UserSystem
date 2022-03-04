13a0bda0dac3ed2a735d584c5615a6ad41c19dc1
volkangurbuz <volkangurbuz16@gmail.com>
2022-03-04 15:38:15 +0100

    adding a re-usable card component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,25 @@
 import React from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 
+const AddUser = props => {
 
-const AddUser = props =>{
-
-    const addUserHandler = (event) =>{
+    const addUserHandler = (event) => {
         event.preventDefault();
     };
 
     //which label belongs to which inputs for screen readers htmlFor
     return (
-    <form onSubmit={addUserHandler}>
-        <label htmlFor="username" >Username</label>
-        <input id="username" type="text" />
-        <label htmlFor="age" >Age (Years)</label>
-        <input id="age" type="number" />
-        <button type="submit">Add User</button>
-    </form>);
+
+        <Card className={classes.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text"/>
+                <label htmlFor="age">Age (Years)</label>
+                <input id="age" type="number"/>
+                <button type="submit">Add User</button>
+            </form>
+        </Card>);
 
 };
 
